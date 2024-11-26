a4698f7cf68c174ca630fa6d59c1cc8be49119dd
Muhammad-Mohsin0 <mohsin.signin00@gmai.com>
2024-11-26 23:31:26 +0500

    biuling

--- next.config.mjs
+++ next.config.mjs
@@ -1,32 +1,19 @@
-// // import { withSentryConfig } from "@sentry/nextjs";
-
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   // Add any other Next.js configurations here
-// };
-
-// // const sentryConfig = {
-// //   silent: true,
-// //   org: "mosi-tp",
-// //   project: "portfolio-nextjs",
-// //   widenClientFileUpload: true,
-// //   transpileClientSDK: true,
-// //   hideSourceMaps: true,
-// //   disableLogger: true,
-// //   automaticVercelMonitors: true,
-// // };
-
-// // export default withSentryConfig(nextConfig, sentryConfig);
-// export default nextConfig;
-
 import { withSentryConfig } from "@sentry/nextjs";
 
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  // your other Next.js configurations
+  // Add any other Next.js configurations here
 };
 
-export default withSentryConfig(nextConfig, {
+const sentryConfig = {
   silent: true,
   org: "mosi-tp",
   project: "portfolio-nextjs",
-});
+  widenClientFileUpload: true,
+  transpileClientSDK: true,
+  hideSourceMaps: true,
+  disableLogger: true,
+  automaticVercelMonitors: true,
+};
+
+export default withSentryConfig(nextConfig, sentryConfig);
