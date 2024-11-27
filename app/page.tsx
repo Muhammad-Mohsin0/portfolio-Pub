78ae0618f4f2ce1978b657b1bc46d66a7900f2c1
Muhammad-Mohsin0 <mohsin.signin00@gmai.com>
2024-11-27 13:54:14 +0500

    making

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,3 @@
-"use client";
-
-import { navItems } from "@/data";
-import Hero from "@/components/Hero";
-import Grid from "@/components/Grid";
-import Footer from "@/components/Footer";
-("use client");
-
 import dynamic from "next/dynamic";
 
 const ClientPage = dynamic(() => import("./client-page"), {
